a0e3d6788698dcbaf68c50f2a2f679cd4eb5e690
sierkinhane <sierkinhane@gmail.com>
2022-07-26 17:29:53 +0800

    add code for background cues refinement

--- WSSS/PoolNet/train.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-OMP_NUM_THREADS=16 CUDA_VISIBLE_DEVICES=5 python main_voc.py --arch resnet --mode train --train_root /data/xjheng/data/VOC2012/ --pseudo_root ../CFC-WSSSv2/experiments/predictions/c2am-voc2012-7@train@scale=0.5,1.0,1.5,2.0@cam_inference_crf=10-soft/
-# you can optionly change the -lr and -wd params
